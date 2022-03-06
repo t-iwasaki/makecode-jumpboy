66e268a8f81afb9f3c5fcb0aefdf166ab65dd743
t-iwasaki <takaiwa@gmail.com>
2022-03-07 00:55:28 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ tilemap.g.ts
@@ -0,0 +1,90 @@
+// 自動生成されたコードです。変更しないでください。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "レベル1":
+            case "レベル1":return tiles.createTilemap(hex`1000070000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001020302030206060603050107040505`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "rainbow":
+            case "tile1":return tile1;
+            case "transparency16":return transparency16;
+            case "rainbow13":
+            case "tile15":return tile15;
+            case "rainbow1":
+            case "tile9":return tile9;
+            case "rainbow2":
+            case "tile10":return tile10;
+            case "rainbow3":
+            case "tile11":return tile11;
+            case "rainbow4":
+            case "tile12":return tile12;
+            case "rainbow5":
+            case "tile13":return tile13;
+            case "rainbow6":
+            case "tile14":return tile14;
+            case "rainbow12":
+            case "tile8":return tile8;
+            case "rainbow0":
+            case "tile2":return tile2;
+            case "rainbow7":
+            case "tile3":return tile3;
+            case "rainbow8":
+            case "tile4":return tile4;
+            case "rainbow9":
+            case "tile5":return tile5;
+            case "rainbow10":
+            case "tile6":return tile6;
+            case "rainbow11":
+            case "tile7":return tile7;
+        }
+        return null;
+    })
+
+}
+// 自動生成されたコードです。変更しないでください。
